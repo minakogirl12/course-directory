d5babe804ff4c057e141f8c96652b30143087418
drobinson <30643831+minakogirl12@users.noreply.github.com>
2021-01-06 15:03:34 -0500

    Updated
    Prevent 404 error on homepage

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ import NotFound from './NotFound';
 import Featured from './Featured';
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename="/course-directory">
     <div className="container">
     <Header />
     <Switch>
